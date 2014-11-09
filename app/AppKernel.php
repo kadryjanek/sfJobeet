b0d695ed66330dd6ced5d3a3fdb6942d5d21f0d4
Kamil Adryjanek <kamil.adryjanek@gmail.com>
2014-11-09 16:09:54 +0100

    DoctrineMigrations bundle installed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,12 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Bootcamp\LayoutBundle\BootcampLayoutBundle(),
             new Bootcamp\JobeetBundle\BootcampJobeetBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
