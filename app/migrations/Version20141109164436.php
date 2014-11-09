65defce438ef12dfb65293f4152a51afe7bb7ddf
Kamil Adryjanek <kamil.adryjanek@gmail.com>
2014-11-09 16:47:07 +0100

    Migrations updated

---
+++ app/migrations/Version20141109164436.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Application\Migration;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20141109164436 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('ALTER TABLE Job ADD slug VARCHAR(255) NOT NULL, CHANGE position position VARCHAR(100) NOT NULL, CHANGE location location VARCHAR(155) NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('ALTER TABLE Job DROP slug, CHANGE position position VARCHAR(255) NOT NULL, CHANGE location location VARCHAR(255) NOT NULL');
+    }
+}
